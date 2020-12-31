68f81a51b371699669377395da8b3c8193edb7f6
hritikchokker <hchokker7@gmail.com>
2021-01-01 00:09:16 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  otpInput = 5;
+  showComp = true;
+  setNewInput(num: any) {
+    if (+num % 1 !== 0) {
+      return;
+    }
+    this.showComp = false;
+    this.otpInput = parseInt(num);
+    setTimeout(() => {
+      this.showComp = true;
+    });
+  }
+  
 }
