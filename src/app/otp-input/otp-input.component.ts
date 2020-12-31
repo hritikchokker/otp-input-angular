5a74eafc78b74630244f11e0e62fde857fcaf0ae
hritikchokker <hchokker7@gmail.com>
2020-12-31 23:57:32 +0530

    4 files modified

--- src/app/otp-input/otp-input.component.ts
+++ src/app/otp-input/otp-input.component.ts
@@ -1,43 +1,39 @@
-import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-import { InputFieldComponent } from './input-field/input-field.component';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnInit,
+  ViewChild
+} from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-otp-input',
-  templateUrl: './otp-input.component.html',
-  styleUrls: ['./otp-input.component.css']
+  selector: "app-otp-input",
+  templateUrl: "./otp-input.component.html",
+  styleUrls: ["./otp-input.component.css"]
 })
 export class OtpInputComponent implements OnInit, AfterViewInit {
-
-
   totalFields = 5;
   @Input() set totalDigits(num: number) {
     for (let i = 0; i < num; i++) {
-      this.loopTo.push(this.makeid(5))
+      this.loopTo.push(this.makeid(5));
     }
-
-  };
-  loopTo: any[] = []
+  }
+  loopTo: any[] = [];
   @Input() isOnlyNumbers = true;
 
   createForm() {
-
     // this.form = this._fb.group({});
   }
   controlsNameArr: string[] = [];
 
-
-  handleValueChanges() {
-
-
-  }
-  ngOnInit(): void {
-  }
+  handleValueChanges() {}
+  ngOnInit(): void {}
 
   makeid(length: any) {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const charactersLength = characters.length;
     for (let i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -45,8 +41,5 @@ export class OtpInputComponent implements OnInit, AfterViewInit {
     return result;
   }
 
-  ngAfterViewInit() {
-  }
-
-
+  ngAfterViewInit() {}
 }
--- src/app/otp-input/otp-input.module.ts
+++ src/app/otp-input/otp-input.module.ts
@@ -1,14 +1,13 @@
-import { CommonModule } from '@angular/common';
-import { InputFieldComponent } from './input-field/input-field.component';
-import { NgModule } from '@angular/core';
-import { OtpInputComponent } from './otp-input.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from "@angular/common";
+
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { InputFieldComponent } from "./input-field/input-field.component";
+import { OtpInputComponent } from "./otp-input.component";
 
 @NgModule({
   declarations: [OtpInputComponent, InputFieldComponent],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule
-  ], exports: [OtpInputComponent]
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [OtpInputComponent]
 })
-export class OtpInputModule { }
+export class OtpInputModule {}
